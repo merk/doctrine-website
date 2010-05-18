00b9a74e738b9434092b34cb673ef60193059011
Jonathan H. Wage <jonwage@gmail.com>
2010-05-18 13:44:41 -0400

    Fixing 404 error

--- apps/frontend/modules/main/templates/error404Success.php
+++ apps/frontend/modules/main/templates/error404Success.php
@@ -1,7 +1,5 @@
-<?php use_helper('I18N'); ?>
-
-<h2><?php echo __('Oops! The page you asked for could not be found.'); ?></h2>
+<h2>Oops! The page you asked for could not be found.</h2>
 
 <p><?php echo sfContext::getInstance()->getRequest()->getUri(); ?></p>
 
-<p><?php echo button_to(__('Go Back'), '#', array('onClick' => 'history.go(-1);')); ?></p>
+<p><input type="button" value="Go Back" onClick="javascript: history.go(-1);" /></p>
